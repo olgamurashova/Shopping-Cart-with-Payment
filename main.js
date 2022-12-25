a5bf026b59c711822c0da4aedf9604a7edf1d34f
Olga Murashova <om.dreyfus@gmail.com>
2022-12-25 04:10:02 -0500

    main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//All elements
+
+const payAmountBtn = document.querySelector('#payAmount');
+
+const decrementBtn = document.querySelectorAll('#decrement');
+
+const quantiytEl = document.querySelectorAll('#quantity');
+
+const incrementBtn = document.querySelectorAll('#increment');
+
+const price = document.querySelectorAll('#price');
+
+const subtotal = document.querySelectorAll('#subtotal');
+
+const tax = document.querySelectorAll('#tax');
+
+const shipping = document.querySelectorAll('#shipping');
+
+const total = document.querySelectorAll('#total');
