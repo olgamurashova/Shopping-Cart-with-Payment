254499ba4cf0f8479bb2792dbd62f9a2b28a848f
Olga Murashova <om.dreyfus@gmail.com>
2022-12-26 12:12:26 -0500

    main.js

--- main.js
+++ main.js
@@ -19,3 +19,24 @@ const tax = document.querySelectorAll('#tax');
 const shipping = document.querySelectorAll('#shipping');
 
 const total = document.querySelectorAll('#total');
+
+//loop: for add event on multiple "increment" & "decrement" buttons
+
+for (let i = 0; i < incrementBtn.length; i ++) {
+    incrementBtn[i].addEventListener("click", function () {
+
+        //collect the value of 'quantity' textContent based on clicked "increment" button sibling
+        let increment = Number(this.previousElementSibling.textContent);
+
+        //plus "increment" variable value by 1
+        increment++;
+
+        //show "increment" variable value on 'quantity' element based on clicked "increment" button sibling
+        this.previousElementSibling.textContent = increment;
+
+    });
+
+    decrementBtn[i].addEventListener9("click", function () {
+
+    });
+}
