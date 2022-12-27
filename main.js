77acd79ba4a4c6bc9aab6438bc3f86c6f281c123
Olga Murashova <om.dreyfus@gmail.com>
2022-12-27 13:10:02 -0500

    main.js edits

--- main.js
+++ main.js
@@ -1,90 +1,88 @@
 'use strict';
 
-//All elements
-
+// all initial elements
 const payAmountBtn = document.querySelector('#payAmount');
-
 const decrementBtn = document.querySelectorAll('#decrement');
-
-const quantitytEl = document.querySelectorAll('#quantity');
-
+const quantityElem = document.querySelectorAll('#quantity');
 const incrementBtn = document.querySelectorAll('#increment');
+const priceElem = document.querySelectorAll('#price');
+const subtotalElem = document.querySelector('#subtotal');
+const taxElem = document.querySelector('#tax');
+const totalElem = document.querySelector('#total');
 
-const price = document.querySelectorAll('#price');
 
-const subtotalEl = document.querySelector('#subtotal');
+// loop: for add event on multiple `increment` & `decrement` button
+for (let i = 0; i < incrementBtn.length; i++) {
 
-const taxEl = document.querySelector('#tax');
+  incrementBtn[i].addEventListener('click', function () {
 
-const shipping = document.querySelector('#shipping');
+    // collect the value of `quantity` textContent,
+    // based on clicked `increment` button sibling.
+    let increment = Number(this.previousElementSibling.textContent);
 
-const totalEl = document.querySelector('#total');
+    // plus `increment` variable value by 1
+    increment++;
 
-//loop: for add event on multiple "increment" & "decrement" buttons
+    // show the `increment` variable value on `quantity` element
+    // based on clicked `increment` button sibling.
+    this.previousElementSibling.textContent = increment;
 
-for (let i = 0; i < incrementBtn.length; i ++) {
-    incrementBtn[i].addEventListener("click", function () {
+    totalCalc();
 
-        //collect the value of 'quantity' textContent based on clicked "increment" button sibling
-        let increment = Number(this.previousElementSibling.textContent);
+  });
 
-        //plus "increment" variable value by 1
-        increment++;
 
-        //show "increment" variable value on 'quantity' element based on clicked "increment" button sibling
-        this.previousElementSibling.textContent = increment;
-        totalCalc();
+  decrementBtn[i].addEventListener('click', function () {
 
-    });
+    // collect the value of `quantity` textContent,
+    // based on clicked `decrement` button sibling.
+    let decrement = Number(this.nextElementSibling.textContent);
 
-    decrementBtn[i].addEventListener("click", function () {
-         //collect the value of 'quantity' textContent based on clicked "decrement" button sibling
-         let decrement = Number(this.nextElementSibling.textContent);
+    // minus `decrement` variable value by 1 based on condition
+    decrement <= 1 ? 1 : decrement--;
 
-         //minus 'decrement' variable value by 1 based on condition
-         decrement <= 1 ? 1 : decrement--;
+    // show the `decrement` variable value on `quantity` element
+    // based on clicked `decrement` button sibling.
+    this.nextElementSibling.textContent = decrement;
 
-         //show "decrement" variable value on 'quantity' element based on clicked "decrement" button sibling
-         this.nextElementSibling.textContent = decrement;
-         totalCalc();
+    totalCalc();
 
-    });
+  });
+
 }
 
-//function for calculating total amount of product price
 
-const totalCalc = function() {
-    //declare all initial variables 
 
-    let tax = 0.05;
-    let subtotal = 0;
-    let totalTax = 0;
-    let total = 0;
+// function: for calculating total amount of product price
+const totalCalc = function () {
 
-    //loop for calculating 'subtotal' value from every single element
+  // declare all initial variable
+  const tax = 0.05;
+  let subtotal = 0;
+  let totalTax = 0;
+  let total = 0;
 
-    for (let i = 0; i < quantitytEl.length; i++) {
-        subtotal += Number(quantitytEl[i].textContent) * Number(price[i].textContent);
+  // loop: for calculating `subtotal` value from every single product
+  for (let i = 0; i < quantityElem.length; i++) {
 
-    }
+    subtotal += Number(quantityElem[i].textContent) * Number(priceElem[i].textContent);
 
-//show the 'subtotal' variable value on 'subtotal' element 
-    subtotalEl.textContent = subtotal.toFixed(2);
+  }
 
-//claculating subtotal tax
+  // show the `subtotal` variable value on `subtotalElem` element
+  subtotalElem.textContent = subtotal.toFixed(2);
 
-    totalTax = subtotal * tax;
+  // calculating the `totalTax`
+  totalTax = subtotal * tax;
 
-//show the totaltax on tax element 
-    taxEl.textContent = totalTax.toFixed(2);
+  // show the `totalTax` on `taxElem` element
+  taxElem.textContent = totalTax.toFixed(2);
 
-//calculating the total 
+  // calcualting the `total`
+  total = subtotal + totalTax;
 
-    total = subtotal + totalTax;
-
-//show the 'total' variable value on 'total' element & 'payAmountBtn' 
-
-    totalEl.textContent = total.toFixed(2);
-    payAmountBtn.textContent = total.toFixed(2);
+  // show the `total` variable value on `totalElem` & `payAmountBtn` element
+  totalElem.textContent = total.toFixed(2);
+  payAmountBtn.textContent = total.toFixed(2);
 
 }
